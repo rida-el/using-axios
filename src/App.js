4599ae7488b712234830430c6a67ae2ba2261efa
rida-el <rida.el99@gmail.com>
2022-08-07 02:56:28 +0100

    add api link

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import react, { useEffect } from "react";
+import axios from "axios";
+import "./App.css";
+
+const fetchdata = () => {
+  return axios
+    .get("https://62eeefbcf5521ecad57c88cd.mockapi.io/projects")
+    .then((res) => {
+      console.log(res);
+    })
+    .catch((err) => {
+      console.error(err);
+    });
+};
 
 function App() {
+  useEffect(() => {
+    fetchdata();
+  }, []);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Using axios for fetching data from api</h1>
     </div>
   );
 }
